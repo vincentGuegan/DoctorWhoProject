1dd0290d63fee891c3f19ffb06a9d361ab284588
Vincent Guegan <gueganvincent@gmail.com>
2019-06-24 18:39:00 +0200

    correction indentation et nom de Peter Capaldi

--- src/main/java/com/doctorwho/DoctorWhoProject/DoctorWhoProjectApplication.java
+++ src/main/java/com/doctorwho/DoctorWhoProject/DoctorWhoProjectApplication.java
@@ -11,48 +11,48 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 public class DoctorWhoProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DoctorWhoProjectApplication.class, args);
-	}
+public static void main(String[] args) {
+	SpringApplication.run(DoctorWhoProjectApplication.class, args);
+}
 
-	@RequestMapping("/")
-	@ResponseBody
+@RequestMapping("/")
+@ResponseBody
 	
-    public String index() {
-		return
+public String index() {
+	return
 	"<ul>" + 
 		"<li><a href=\"/doctor/1\">hartnell</a></li>" + 
 		"<li><a href=\"/doctor/2\">tennant</a></li>" + 
 		"<li><a href=\"/doctor/3\">whittaker</a></li>" + 
-		"<li><a href=\"/doctor/4\">capalbi</a></li>" + 
+		"<li><a href=\"/doctor/4\">capaldi</a></li>" + 
 	"</ul>";
-    }
+}
 
-	@RequestMapping("/doctor/1")
-	@ResponseBody
+@RequestMapping("/doctor/1")
+@ResponseBody
 
-	public String hartnell() {
-        return "William Hartnell";
-    }
+public String hartnell() {
+    return "William Hartnell";
+}
 
-	@RequestMapping("/doctor/2")
-	@ResponseBody
+@RequestMapping("/doctor/2")
+@ResponseBody
 
-	public String tennant() {
-        return "David Tennant";
-    }
+public String tennant() {
+    return "David Tennant";
+}
 
-	@RequestMapping("/doctor/3")
-	@ResponseBody
+@RequestMapping("/doctor/3")
+@ResponseBody
 
-	public String whittaker() {
-        return "Jodie Whittaker";
-    }
+public String whittaker() {
+    return "Jodie Whittaker";
+}
 
-	@RequestMapping("/doctor/4")
-	@ResponseBody
+@RequestMapping("/doctor/4")
+@ResponseBody
 
-	public String capalbi() {
-        return "Peter Capalbi";
+public String capaldi() {
+	return "Peter Capalbi";
     }
 }
